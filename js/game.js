471eb93609b5825301eea01ce759c191d5935e49
your name here <ahrampy@gmail.com>
2020-02-25 23:14:42 -0800

    isolate score logic

--- js/game.js
+++ js/game.js
@@ -4,41 +4,18 @@ window.addEventListener("load", init, false);
 
 var towerTime;
 var tutorial;
+var score;
 
 function init() {
   towerTime = new Game();
   tutorial = new Tutorial();
-  loadFireBase();
+  score = new Score();
   window.setTimeout(animate, 100);
 }
 
 function animate() {
   towerTime.run();
   window.requestAnimationFrame(animate);
-}
-
-function loadFireBase() {
-  var firebaseConfig = {
-    apiKey: "AIzaSyCUl00bvLb_3Ytr6Wj_L-XIp-bVX4Yb8b0",
-    authDomain: "tower-time.firebaseapp.com",
-    databaseURL: "https://tower-time.firebaseio.com",
-    projectId: "tower-time",
-    storageBucket: "tower-time.appspot.com",
-    messagingSenderId: "114254327718",
-    appId: "1:114254327718:web:7d6db95c7f120f99452967",
-    measurementId: "G-0BEHEHSDDL"
-  };
-  firebase.initializeApp(firebaseConfig);
-}
-
-function writeUserData(id, name, score) {
-  firebase
-    .database()
-    .ref("scores/" + id)
-    .set({
-      name: name,
-      score: score
-    });
 }
  
 function round5(x) {
---
+++ js/score.js
@@ -0,0 +1,32 @@
+"use strict";
+
+class Score {
+  constructor() {
+    this.loadFireBase();
+  }
+ 
+  loadFireBase(){
+    var firebaseConfig = {
+      apiKey: "AIzaSyCUl00bvLb_3Ytr6Wj_L-XIp-bVX4Yb8b0",
+      authDomain: "tower-time.firebaseapp.com",
+      databaseURL: "https://tower-time.firebaseio.com",
+      projectId: "tower-time",
+      storageBucket: "tower-time.appspot.com",
+      messagingSenderId: "114254327718",
+      appId: "1:114254327718:web:7d6db95c7f120f99452967",
+      measurementId: "G-0BEHEHSDDL"
+    };
+    firebase.initializeApp(firebaseConfig);
+  }
+
+  writeUserData(name, score) {
+    firebase
+      .database()
+      .ref("scores/" + Date.now())
+      .set({
+        name: name,
+        score: score
+      });
+  }
+
+}
