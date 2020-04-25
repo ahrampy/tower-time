087e4ae7f196962eb72ba8798caf5d03ab99cc16
adrian_rampy <ahrampy@gmail.com>
2020-04-25 04:16:18 -0700

    add tower sprites

--- js/sprites.js
+++ js/sprites.js
@@ -1,8 +1,13 @@
 "use strict";
 
+const towerSprites = new Image()
+const attackSprites = new Image()
 const creepSprites = {}
 
+towerSprites.src = "./images/towers/towers.png"
+attackSprites.src = "./images/towers/attacks.png"
 for(let i = 0; i < 5; i++) {
   creepSprites[i] = new Image;
   creepSprites[i].src = `./images/creeps/creep-${i}.png`;
 }
+
