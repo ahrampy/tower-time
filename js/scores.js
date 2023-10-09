88eea4c05bb7e3d8ac579d523f32bf89d342d0b6
Adrian Rampy <ahrampy@gmail.com>
2023-10-08 21:42:55 -0700

    add env

--- js/scores.js
+++ js/scores.js
@@ -1,139 +1,139 @@
 export default class Scores {
-  constructor(dom, game) {
-    this.dom = dom;
-    this.game = game;
-    this.firebase = this.loadFireBase();
-  }
+	constructor(dom, game) {
+		this.dom = dom;
+		this.game = game;
+		this.firebase = this.loadFireBase();
+	}
 
-  loadFireBase() {
-    var firebaseConfig = {
-      apiKey: "AIzaSyCUl00bvLb_3Ytr6Wj_L-XIp-bVX4Yb8b0",
-      authdomain: "tower-time.firebaseapp.com",
-      databaseURL: "https://tower-time.firebaseio.com",
-      projectId: "tower-time",
-      storageBucket: "tower-time.appspot.com",
-      messagingSenderId: "114254327718",
-      appId: "1:114254327718:web:7d6db95c7f120f99452967",
-      measurementId: "G-0BEHEHSDDL",
-    };
-    return firebase.initializeApp(firebaseConfig);
-  }
+	loadFireBase() {
+		const firebaseConfig = {
+			apiKey: process.env.firebaseApiKey,
+			authdomain: "tower-time.firebaseapp.com",
+			databaseURL: "https://tower-time.firebaseio.com",
+			projectId: "tower-time",
+			storageBucket: "tower-time.appspot.com",
+			messagingSenderId: process.env.firebaseSenderId,
+			appId: process.env.firebaseAppId,
+			measurementId: process.env.firebaseMeasurementId,
+		};
+		return firebase.initializeApp(firebaseConfig);
+	}
 
-  update(name, score) {
-    this.firebase.auth().signInAnonymously();
-    this.firebase.database().ref("scores").push({
-      name: name,
-      score: score,
-    });
-  }
+	update(name, score) {
+		this.firebase.auth().signInAnonymously();
+		this.firebase.database().ref("scores").push({
+			name: name,
+			score: score,
+		});
+	}
 
-  handleScores(highscores) {
-    const rankList = document.createElement("ul");
-    const scoreList = document.createElement("ul");
-    const nameList = document.createElement("ul");
+	handleScores(highscores) {
+		const rankList = document.createElement("ul");
+		const scoreList = document.createElement("ul");
+		const nameList = document.createElement("ul");
 
-    let lowestShowing;
+		let lowestShowing;
 
-    highscores.on("value", (snap) => {
-      const data = snap.val();
-      const idArr = Object.keys(data);
-      const scores = [];
+		highscores.on("value", (snap) => {
+			const data = snap.val();
+			const idArr = Object.keys(data);
+			const scores = [];
 
-      for (let i = 0; i < idArr.length; i++) {
-        scores.push([data[idArr[i]].score, data[idArr[i]].name]);
-      }
+			for (let i = 0; i < idArr.length; i++) {
+				scores.push([data[idArr[i]].score, data[idArr[i]].name]);
+			}
 
-      this.sortScores(scores);
+			this.sortScores(scores);
 
-      let rankCount = 1;
-      scores.forEach((score) => {
-        const rankLi = document.createElement("li");
-        const scoreLi = document.createElement("li");
-        const nameLi = document.createElement("li");
-        rankLi.innerText = rankCount++;
-        scoreLi.innerText = score[0];
-        nameLi.innerText = score[1];
-        rankList.appendChild(rankLi);
-        scoreList.appendChild(scoreLi);
-        nameList.appendChild(nameLi);
-      });
-      lowestShowing = scores[9][0];
-    });
+			let rankCount = 1;
+			scores.forEach((score) => {
+				const rankLi = document.createElement("li");
+				const scoreLi = document.createElement("li");
+				const nameLi = document.createElement("li");
+				rankLi.innerText = rankCount++;
+				scoreLi.innerText = score[0];
+				nameLi.innerText = score[1];
+				rankList.appendChild(rankLi);
+				scoreList.appendChild(scoreLi);
+				nameList.appendChild(nameLi);
+			});
+			lowestShowing = scores[9][0];
+		});
 
-    const lists = [rankList, scoreList, nameList];
-    ["RANK", "SCORE", "NAME"].forEach((column, i) => {
-      const div = document.createElement("div");
-      div.classList.add("score-div");
-      const rankTitle = document.createElement("h2");
-      rankTitle.innerText = column;
-      div.appendChild(rankTitle);
-      div.appendChild(lists[i]);
-      this.dom.scores.appendChild(div);
-    });
+		const lists = [rankList, scoreList, nameList];
+		["RANK", "SCORE", "NAME"].forEach((column, i) => {
+			const div = document.createElement("div");
+			div.classList.add("score-div");
+			const rankTitle = document.createElement("h2");
+			rankTitle.innerText = column;
+			div.appendChild(rankTitle);
+			div.appendChild(lists[i]);
+			this.dom.scores.appendChild(div);
+		});
 
-    if (this.game.score >= lowestShowing) {
-      const form = document.createElement("form");
-      const input = document.createElement("input");
-      input.classList.add("name-input");
-      input.placeholder = "ADD NAME";
-      input.maxLength = 3;
-      form.appendChild(input);
-      this.dom.terminal.appendChild(form);
-      form.addEventListener("submit", (event) =>
-        this.addScore(event, lowestShowing, form)
-      );
-    } else {
-      this.addNewGame();
-    }
-  }
+		if (this.game.score >= lowestShowing) {
+			const form = document.createElement("form");
+			const input = document.createElement("input");
+			input.classList.add("name-input");
+			input.placeholder = "ADD NAME";
+			input.maxLength = 3;
+			form.appendChild(input);
+			this.dom.terminal.appendChild(form);
+			form.addEventListener("submit", (event) =>
+				this.addScore(event, lowestShowing, form)
+			);
+		} else {
+			this.addNewGame();
+		}
+	}
 
-  addScore(event, lowestShowing, form) {
-    event.preventDefault();
-    const name = document.querySelector(".name-input");
-    const scoreList = document.querySelectorAll(".score-div");
-    if (this.game.score >= lowestShowing) {
-      scoreList.forEach((div) => {
-        div.querySelector("ul").innerHTML = "";
-      });
-    }
-    name.style.visibility = "hidden";
-    if (this.game.f === this.game.score) {
-      this.update(name.value.toUpperCase(), this.game.f);
-    } else {
-      window.location.reload(false);
-    }
-    this.dom.terminal.removeChild(form);
-    this.addNewGame();
-  }
+	addScore(event, lowestShowing, form) {
+		event.preventDefault();
+		const name = document.querySelector(".name-input");
+		const scoreList = document.querySelectorAll(".score-div");
+		if (this.game.score >= lowestShowing) {
+			scoreList.forEach((div) => {
+				div.querySelector("ul").innerHTML = "";
+			});
+		}
+		name.style.visibility = "hidden";
+		if (this.game.f === this.game.score) {
+			this.update(name.value.toUpperCase(), this.game.f);
+		} else {
+			window.location.reload(false);
+		}
+		this.dom.terminal.removeChild(form);
+		this.addNewGame();
+	}
 
-  addNewGame() {
-    const btn = document.createElement("button");
-    btn.classList.add("new-game");
-    btn.innerHTML = "NEW GAME";
-    btn.addEventListener(
-      "click",
-      (e) => {
-        e.preventDefault();
-        this.game.actions.newGame();
-      },
-      false
-    );
-    this.dom.terminal.appendChild(btn);
-  }
+	addNewGame() {
+		const btn = document.createElement("button");
+		btn.classList.add("new-game");
+		btn.innerHTML = "NEW GAME";
+		btn.addEventListener(
+			"click",
+			(e) => {
+				e.preventDefault();
+				this.game.actions.newGame();
+			},
+			false
+		);
+		this.dom.terminal.appendChild(btn);
+	}
 
-  sortScores(arr) {
-    let swapped;
-    do {
-      swapped = false;
-      for (let i = 0; i < arr.length - 1; i++) {
-        if (arr[i][0] < arr[i + 1][0]) {
-          let tmp = arr[i];
-          arr[i] = arr[i + 1];
-          arr[i + 1] = tmp;
-          swapped = true;
-        }
-      }
-    } while (swapped);
-    return arr;
-  }
+	sortScores(arr) {
+		let swapped;
+		do {
+			swapped = false;
+			for (let i = 0; i < arr.length - 1; i++) {
+				if (arr[i][0] < arr[i + 1][0]) {
+					let tmp = arr[i];
+					arr[i] = arr[i + 1];
+					arr[i + 1] = tmp;
+					swapped = true;
+				}
+			}
+		} while (swapped);
+		return arr;
+	}
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,25 @@
-var path = require("path");
-var webpack = require("webpack");
+const path = require("path");
+const dotenv = require("dotenv-webpack");
+
 module.exports = {
-  entry: "./js/main.js",
-  output: {
-    path: path.resolve(__dirname, "js/"),
-    filename: "bundle.js",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"],
-          },
-        },
-      },
-    ],
-  },
+	entry: "./js/main.js",
+	output: {
+		path: path.resolve(__dirname, "js/"),
+		filename: "bundle.js",
+	},
+	module: {
+		rules: [
+			{
+				test: /\.m?js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ["@babel/preset-env"],
+					},
+				},
+			},
+		],
+	},
+	plugins: [new dotenv()],
 };
