f783de3fce8abb05954a6b6bf9a6426d71d58ccd
Adrian Rampy <ahrampy@gmail.com>
2023-10-08 23:20:10 -0700

    set fps

--- js/main.js
+++ js/main.js
@@ -5,22 +5,23 @@ import Scores from "./scores";
 
 window.addEventListener("load", init);
 
+const targetFPS = 60;
 var sprites = new Sprites();
 var dom, game, scores, animation;
 
 function init() {
-  dom = dom ? dom : new DomHandler();
+  dom = dom ?? new DomHandler();
   game = new Game(dom, sprites);
   scores
     ? ((scores.game = game), (scores.dom = dom))
     : (scores = new Scores(dom, game));
   game.actions.scores = scores;
   game.actions.init = init;
-  window.cancelAnimationFrame(animation);
+  window.clearTimeout(animation);
   window.setTimeout(animate, 100);
 }
 
 function animate() {
   game.run();
-  animation = window.requestAnimationFrame(animate);
+  animation = window.setTimeout(animate, 1000 / targetFPS);
 }
