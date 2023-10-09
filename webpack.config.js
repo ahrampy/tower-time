c0ef9ba0057b38bcc7be70b3873349d7877d5b2f
Adrian Rampy <ahrampy@gmail.com>
2023-10-08 22:50:20 -0700

    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -7,18 +7,12 @@ module.exports = {
 		path: path.resolve(__dirname, "./dist"),
 		filename: "bundle.js",
 	},
-	mode: "none",
+	mode: "production",
 	module: {
 		rules: [
 			{
 				test: /\.m?js$/,
-				exclude: /(node_modules|bower_components)/,
-				use: {
-					loader: "babel-loader",
-					options: {
-						presets: ["@babel/preset-env"],
-					},
-				},
+				exclude: /(node_modules)/,
 			},
 		],
 	},
